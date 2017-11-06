e61080a0534a1d9b95d33edba086f2ba90e6c5db
Danishkhan007 <danishkhan.jamia@gmail.com>
2017-11-06 23:46:35 +0530

    NumberPad

--- Utilities/Utilities/Classes/View/TextWithLabelCell.m
+++ Utilities/Utilities/Classes/View/TextWithLabelCell.m
@@ -22,10 +22,28 @@
     _textfield.tag = [[dictionary objectForKey:@"index"] intValue];
     
     UIColor* placeHolderColor = (_textfield.tag == EUserName || _textfield.tag == EPassword) ? [UIColor whiteColor] : [UIColor lightGrayColor];
-    
     _textfield.attributedPlaceholder = [[NSAttributedString alloc] initWithString:[dictionary objectForKey:@"placeholder"] attributes:@{NSForegroundColorAttributeName:placeHolderColor }];
 
     _textfield.secureTextEntry = (_textfield.tag == EPassword) ? YES : NO;
+    _textfield.autocorrectionType = UITextAutocorrectionTypeNo;
+    
+    if (_textfield.tag == EContact) {
+        _textfield.keyboardType = UIKeyboardTypeNumberPad;
+        [self setNumberPadWithDoneBtn];
+    }
+}
+
+- (void)setNumberPadWithDoneBtn {
+    UIToolbar* numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, self.bounds.size.width,40)];
+    numberToolbar.barStyle = UIStatusBarStyleDefault;
+    
+    numberToolbar.items = @[ [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],[[UIBarButtonItem alloc]initWithTitle:@"Done" style:UIBarButtonItemStylePlain target:self action:@selector(cancelNumberPad)]];
+    
+    _textfield.inputAccessoryView = numberToolbar;
+}
+
+- (void)cancelNumberPad {
+    [_textfield resignFirstResponder];
 }
 
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
